46ea7f1ed169f56b68873b71199acd7af4f1d0de
João Santos <32807182+jvpdls@users.noreply.github.com>
2024-01-16 16:49:17 -0300

    added file size column

--- popup.js
+++ popup.js
@@ -5,47 +5,132 @@
  * @returns {void}
  */
 function printURLs(urls) {
-  let responseContainer = document.getElementById("response-container");
+  return new Promise((resolve) => {
+    let responseContainer = document.getElementById("response-container");
 
-  let table = document.createElement("table");
+    let table = document.createElement("table");
+    table.id = "response";
 
-  let thead = document.createElement("thead");
-  let headerRow = document.createElement("tr");
-  let headerCell = document.createElement("th");
-  headerCell.textContent = "Stylesheet Links";
-  headerRow.appendChild(headerCell);
-  thead.appendChild(headerRow);
-  table.appendChild(thead);
+    let thead = document.createElement("thead");
+    let headerRow = document.createElement("tr");
+    let headerCell1 = document.createElement("th");
+    headerCell1.textContent = "Stylesheet Links";
+    headerRow.appendChild(headerCell1);
+    let headerCell2 = document.createElement("th");
+    headerCell2.textContent = "File Size (bytes)";
+    headerRow.appendChild(headerCell2);
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
 
-  let tbody = document.createElement("tbody");
+    let tbody = document.createElement("tbody");
 
-  if (urls.length === 0) {
-    let row = document.createElement("tr");
-    let cell = document.createElement("td");
-    cell.textContent = "No stylesheet URLs were found.";
-    row.appendChild(cell);
-    tbody.appendChild(row);
-  } else {
-    urls.forEach((url) => {
+    if (urls.length === 0) {
       let row = document.createElement("tr");
       let cell = document.createElement("td");
-      let link = document.createElement("a");
-      link.href = url;
-      link.target = "_blank";
-      link.textContent = url;
-      cell.appendChild(link);
+      cell.textContent = "No stylesheet URLs were found.";
       row.appendChild(cell);
       tbody.appendChild(row);
+    } else {
+      Promise.all(urls.map(getStylesheetSize)).then((sizes) => {
+        urls.forEach((url, index) => {
+          let size = sizes[index];
+          let row = document.createElement("tr");
+          let cell = document.createElement("td");
+          let link = document.createElement("a");
+          link.href = url;
+          link.target = "_blank";
+          link.textContent = url;
+          cell.appendChild(link);
+          row.appendChild(cell);
+          let sizeCell = document.createElement("td");
+          sizeCell.textContent = size;
+          row.appendChild(sizeCell);
+          tbody.appendChild(row);
+        });
+      });
+    }
+
+    table.appendChild(tbody);
+
+    if (responseContainer.children.length > 0) {
+      responseContainer.removeChild(responseContainer.children[0]);
+    }
+    responseContainer.classList.remove("is-hidden");
+    responseContainer.appendChild(table);
+    resolve();
+  });
+}
+
+/**
+ * Gets the size of an stylesheet.
+ *
+ * @param {string} url - The URL to be analyzed.
+ * @returns {Promise<string>}
+ */
+function getStylesheetSize(url) {
+  return fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.headers.get("content-length");
+      } else {
+        return "N/A";
+      }
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  }
+}
 
-  table.appendChild(tbody);
+/**
+ * Adds a button to the popup that allows the user to download the URLs as a CSV file.
+ *
+ * @returns {void}
+ */
+function addDownloadButton() {
+  let downloadButton = document.createElement("button");
+  downloadButton.type = "button";
+  downloadButton.textContent = "Download as CSV";
+  downloadButton.addEventListener("click", function () {
+    downloadTableAsCSV("response");
+  });
+  document.getElementById("response-container").appendChild(downloadButton);
+}
 
-  if (responseContainer.children.length > 0) {
-    responseContainer.removeChild(responseContainer.children[0]);
+/**
+ * Downloads the given table as a CSV file.
+ *
+ * @param {string} tableId
+ * @param {*} separator
+ */
+function downloadTableAsCSV(tableId, separator = ",") {
+  var rows = document.querySelectorAll("table#" + tableId + " tr");
+  var csv = [];
+  for (var i = 0; i < rows.length; i++) {
+    var row = [],
+      cols = rows[i].querySelectorAll("td, th");
+    for (var j = 0; j < cols.length; j++) {
+      var data = cols[j].innerText
+        .replace(/(\r\n|\n|\r)/gm, "")
+        .replace(/(\s\s)/gm, " ");
+      data = data.replace(/"/g, '""');
+      row.push('"' + data + '"');
+    }
+    csv.push(row.join(separator));
   }
-  responseContainer.classList.remove("is-hidden");
-  responseContainer.appendChild(table);
+  var csvString = csv.join("\n");
+  var filename =
+    "export_" + tableId + "_" + new Date().toLocaleDateString() + ".csv";
+  var link = document.createElement("a");
+  link.style.display = "none";
+  link.setAttribute("target", "_blank");
+  link.setAttribute(
+    "href",
+    "data:text/csv;charset=utf-8," + encodeURIComponent(csvString)
+  );
+  link.setAttribute("download", filename);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 }
 
 /*
@@ -60,7 +145,11 @@ document.addEventListener("DOMContentLoaded", function () {
         tabs[0].id,
         { message: "extract" },
         function (response) {
-          printURLs(response.content);
+          printURLs(response.content).then(() => {
+            if (response.content.length > 0) {
+              addDownloadButton();
+            }
+          });
         }
       );
     });
